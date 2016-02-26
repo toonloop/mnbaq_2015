e3f65870e6fc68f45521a8ea2ba6e3a5193ae2cf
Alexandre Quessy <alexandre@quessy.net>
2016-02-26 16:51:14 -0500

    much progress

--- sh_watch_files_and_copy.sh
+++ sh_watch_files_and_copy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+#
+# XXX Changez ici l'adresse IP de l'ordi art3, qui fait jouer les clips
+#
+
+IP_PLAYER=10.9.1.166
+
+
+
 FROM_DIR=~/Documents/toonloop/default/movies
-DEST=/tmp/yesss
+DEST=art@${IP_PLAYER}:Videos
 
 set -o verbose
 mkdir -p ${DEST} # TODO: we won't need this when we will use scp
 while true;
 do
     CHANGEDFILE=$(inotifywait -e close_write --format %f ${FROM_DIR} )
-    cp ${FROM_DIR}/${CHANGEDFILE} ${DEST}
+    scp ${FROM_DIR}/${CHANGEDFILE} ${DEST}/
 done
